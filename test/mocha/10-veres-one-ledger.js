989ec28e932ebdaeb100ceb3b60073c0624ade09
Manu Sporny <msporny@digitalbazaar.com>
2017-09-02 17:54:06 -0400

    Test for basic ledger agent existence.

--- test/mocha/10-veres-one-ledger.js
+++ test/mocha/10-veres-one-ledger.js
@@ -6,19 +6,25 @@
 
 const async = require('async');
 const bedrock = require('bedrock');
+const config = bedrock.config;
 const brIdentity = require('bedrock-identity');
 const database = require('bedrock-mongodb');
 const expect = global.chai.expect;
 const helpers = require('./helpers');
 const mockData = require('./mock.data');
-const vrLedger = require('../../lib');
+const vrOneLedger = require('../../lib/ledger');
 const uuid = require('uuid/v4');
 
 describe('Veres One Ledger', () => {
-  it('ledger node should exist', done => {
-    done();
+  before(done => {
+    // wait for the ledger to initialize
+    setTimeout(done, 1000);
   });
+
   it('ledger agent should exist', done => {
+    should.exist(vrOneLedger.agent);
+    should.exist(vrOneLedger.agent.node);
     done();
   });
+
 });
