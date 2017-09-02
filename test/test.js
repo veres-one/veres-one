066c1f793fea563a423033f059e41a04b0d0bc86
Manu Sporny <msporny@digitalbazaar.com>
2017-09-02 17:53:14 -0400

    Use JSON-LD document loader from Veres One.

--- test/test.js
+++ test/test.js
@@ -18,29 +18,7 @@ require('bedrock-ledger-validator-equihash');
 require('bedrock-ledger-validator-signature');
 require('bedrock-ledger-storage-mongodb');
 require('../lib');
-
 require('../configs/dev');
-
-bedrock.events.on('bedrock.init', () => {
-  const jsonld = bedrock.jsonld;
-  const mockData = require('./mocha/mock.data');
-
-  const oldLoader = jsonld.documentLoader;
-
-  jsonld.documentLoader = function(url, callback) {
-    if(Object.keys(mockData.ldDocuments).includes(url)) {
-      return callback(null, {
-        contextUrl: null,
-        document: mockData.ldDocuments[url],
-        documentUrl: url
-      });
-    }
-    oldLoader(url, callback);
-  };
-  // override jsonld.documentLoader in brDidClient so this document loader
-  // can be used for did: and https: URLs
-  brDidClient.jsonld.documentLoader = jsonld.documentLoader;
-});
 
 require('bedrock-test');
 bedrock.start();
