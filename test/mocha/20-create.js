be308c17344c7946c7e3d1f2d10d3c38b0a6feee
Manu Sporny <msporny@digitalbazaar.com>
2017-09-05 23:18:40 -0400

    Add test skeleton for DID registration.

--- test/mocha/20-create.js
+++ test/mocha/20-create.js
@@ -8,6 +8,7 @@ const async = require('async');
 const bedrock = require('bedrock');
 const config = bedrock.config;
 const equihashSigs = require('equihash-signature');
+const fs = require('fs');
 const helpers = require('./helpers');
 const jsigs = require('jsonld-signatures');
 const mockData = require('./mock.data');
@@ -20,18 +21,39 @@ const querystring = require('querystring');
 const urlObj = {
   protocol: 'https',
   host: config.server.host,
-  pathname: config['ledger-agent'].routes.agents
+  pathname: '/dids'
 };
 
 // use local JSON-LD processor for signatures
 jsigs.use('jsonld', bedrock.jsonld);
 
 describe.skip('Create DID', () => {
-  const regularActor = mockData.identities.regularUser;
   let ledgerAgent;
 
   it('should allow valid DID to be created', done => {
-    done();
+    const validDdo = bedrock.util.clone(mockData.ddos.valid);
+    const registerEvent = bedrock.util.clone(mockData.events.create);
+    registerEvent.input = [validDdo];
+
+    async.auto({
+      getPrivateKey: callback =>
+        fs.readFile(config['veres-one'].privateKey, 'utf8', callback),
+      sign: ['getPrivateKey', (results, callback) => jsigs.sign(registerEvent, {
+        algorithm: 'LinkedDataSignature2015',
+        privateKeyPem: results.getPrivateKey,
+        creator: config['veres-one'].ddoPublicKey.id
+      }, callback)],
+      register: ['sign', (results, callback) => {
+        request.post({
+          url: config['veres-one'].routes.dids + '/' + validDdo.id,
+          body: results.signEvent
+        }, (err, res) => {
+          should.not.exist(err);
+          res.statusCode.should.equal(201);
+          callback(null, res.headers.location);
+        });
+      }]
+    }, err => done(err));
   });
   it('should prevent DID creation without proof', done => {
     done();
