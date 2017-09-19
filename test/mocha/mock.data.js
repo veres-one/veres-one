8a000ab0dfc171c5ef8455087e31522fbe9a2875
Manu Sporny <msporny@digitalbazaar.com>
2017-09-18 21:14:38 -0400

    Change the way DIDs are created.

--- test/mocha/mock.data.js
+++ test/mocha/mock.data.js
@@ -10,74 +10,13 @@ const helpers = require('./helpers');
 const mock = {};
 module.exports = mock;
 
-mock.ddos = {}
+mock.didDescriptions = {}
 mock.events = {};
 
-mock.ddos.valid = {
-  '@context': 'https://w3id.org/veres-one/v1',
-  id: 'did:v1:215cb1dc-1f44-4695-a07f-97649cad9938',
-  authorization: [{
-    // this entity may update any field in this DDO using any authentication
-    //mechanism understood by the ledger
-    capability: 'UpdateDdo',
-    entity: 'did:v1:215cb1dc-1f44-4695-a07f-97649cad9938'
-  }, {
-    // this entity may update the authenticationCredential field in this DDO
-    // as long as they authenticate with RsaSignature2017
-    entity: 'did:v1:b5f8c320-f7ca-4869-85e6-a1bcbf825b2a',
-    capability: 'UpdateDdo',
-    field: ['authenticationCredential'],
-    permittedProofType: [{
-      proofType: 'RsaSignature2017'
-    }]
-  }, {
-    // anyone may update the authenticationCredential and authorization
-    // fields as long as they provide a specific multi-signature proof
-    capability: 'UpdateDdo',
-    field: ['authenticationCredential', 'authorization'],
-    permittedProofType: [{
-      proofType: 'RsaSignature2017',
-      minimumSignatures: 3,
-      authenticationCredential: [{
-        id: 'did:v1:304ebc3e-7997-4bf4-a915-dd87e8455941/keys/123',
-        type: 'RsaCryptographicKey',
-        owner: 'did:v1:304ebc3e-7997-4bf4-a915-dd87e8455941',
-        publicKeyPem: '-----BEGIN PUBLIC KEY...END PUBLIC KEY-----\r\n'
-      }, {
-        id: 'did:v1:0f22346a-a360-4f3e-9b42-3366e348e941/keys/foo',
-        type: 'RsaCryptographicKey',
-        owner: 'did:v1:0f22346a-a360-4f3e-9b42-3366e348e941',
-        publicKeyPem: '-----BEGIN PUBLIC KEY...END PUBLIC KEY-----\r\n'
-      }, {
-        id: 'did:v1:a8d00377-e9f1-44df-a1b9-55072e13262a/keys/abc',
-        type: 'RsaCryptographicKey',
-        owner: 'did:v1:a8d00377-e9f1-44df-a1b9-55072e13262a',
-        publicKeyPem: '-----BEGIN PUBLIC KEY...END PUBLIC KEY-----\r\n'
-      }]
-    }]
-  }, {
-    // this entity may issue credentials where the 'issuer' field is this
-    // DDO's DID as long as this specific RSA key is used
-    capability: 'IssueCredential',
-    entity: 'did:v1:215cb1dc-1f44-4695-a07f-97649cad9938',
-    permittedProofType: [{
-      proofType: 'RsaSignature2017',
-      authenticationCredential: [{
-        id: 'did:v1:215cb1dc-1f44-4695-a07f-97649cad9938/keys/1',
-        type: 'RsaCryptographicKey',
-        owner: 'did:v1:215cb1dc-1f44-4695-a07f-97649cad9938',
-        publicKeyPem: '-----BEGIN PUBLIC KEY...END PUBLIC KEY-----\r\n'
-      }]
-    }]
-  }],
-  authenticationCredential: [{
-    // this key can be used to authenticate as DID ...9938
-    "id": "did:v1:215cb1dc-1f44-4695-a07f-97649cad9938/keys/1",
-    "type": "RsaCryptographicKey",
-    "owner": "did:v1:215cb1dc-1f44-4695-a07f-97649cad9938",
-    "publicKeyPem": "-----BEGIN PUBLIC KEY...END PUBLIC KEY-----\r\n"
-  }]
-};
+mock.didDescriptions.alpha = helpers.generateDid();
+mock.didDescriptions.beta = helpers.generateDid();
+mock.didDescriptions.gamma = helpers.generateDid();
+mock.didDescriptions.delta = helpers.generateDid();
 
 mock.events.create = {
   '@context': 'https://w3id.org/webledger/v1',
