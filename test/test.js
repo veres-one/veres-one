acc202e1820b785480518d97586aa13ee4081146
Manu Sporny <msporny@digitalbazaar.com>
2017-09-21 23:00:37 -0400

    Align test framework with Veres Delta.

--- test/test.js
+++ test/test.js
@@ -5,19 +5,11 @@
 
 process.env['NODE_PATH'] = '../node_modules';
 const bedrock = require('bedrock');
-const brDidClient = require('bedrock-did-client');
 // NOTE: it is critical that bedrock-protractor be required first so that
 // it can register a bedrock.cli event listener
 require('bedrock-protractor');
-//require('bedrock-permission'); // FIXME
-require('bedrock-ledger-node');
-require('bedrock-ledger-agent');
-require('bedrock-ledger-consensus-uni');
-require('bedrock-ledger-context');
-require('bedrock-ledger-validator-equihash');
-require('bedrock-ledger-validator-signature');
-require('bedrock-ledger-storage-mongodb');
 require('../lib');
+
 require('../configs/dev');
 
 require('bedrock-test');
