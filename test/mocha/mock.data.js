ea40e7644830fffeaf4248b7bfd03474e8148173
Manu Sporny <msporny@digitalbazaar.com>
2018-02-07 22:02:34 -0500

    Fix key generation delays/format for tests.

--- test/mocha/mock.data.js
+++ test/mocha/mock.data.js
@@ -19,16 +19,16 @@ mock.operations = {};
 mock.keys = {};
 
 mock.keys.alpha = helpers.generateDidKeys();
-mock.keys.beta = helpers.generateDidKeys();
-mock.keys.delta = helpers.generateDidKeys();
-mock.keys.epsilon = helpers.generateDidKeys();
-mock.keys.gamma = helpers.generateDidKeys();
+//mock.keys.beta = helpers.generateDidKeys();
+//mock.keys.delta = helpers.generateDidKeys();
+//mock.keys.epsilon = helpers.generateDidKeys();
+//mock.keys.gamma = helpers.generateDidKeys();
 
 mock.didDescriptions.alpha = helpers.generateDid(mock.keys.alpha);
-mock.didDescriptions.beta = helpers.generateDid(mock.keys.beta);
-mock.didDescriptions.delta = helpers.generateDid(mock.keys.delta);
-mock.didDescriptions.epsilon = helpers.generateDid(mock.keys.epsilon);
-mock.didDescriptions.gamma = helpers.generateDid(mock.keys.gamma);
+//mock.didDescriptions.beta = helpers.generateDid(mock.keys.beta);
+//mock.didDescriptions.delta = helpers.generateDid(mock.keys.delta);
+//mock.didDescriptions.epsilon = helpers.generateDid(mock.keys.epsilon);
+//mock.didDescriptions.gamma = helpers.generateDid(mock.keys.gamma);
 
 mock.operations.create = {
   '@context': constants.WEB_LEDGER_CONTEXT_V1_URL,
