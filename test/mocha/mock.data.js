b87507d65aab3b10dbc4f5636164072f1a438382
Manu Sporny <msporny@digitalbazaar.com>
2018-02-09 22:18:11 -0500

    Switch tests to use did-veres-one helper library.

--- test/mocha/mock.data.js
+++ test/mocha/mock.data.js
@@ -5,6 +5,7 @@
 
 const config = require('bedrock').config;
 const constants = config.constants;
+const didv1 = require('did-veres-one');
 const helpers = require('./helpers');
 
 const mock = {};
@@ -13,22 +14,20 @@ module.exports = mock;
 mock.equihashParameterN = 64,
 mock.equihashParameterK = 3,
 
-mock.didDescriptions = {};
+mock.didDocuments = {};
 mock.events = {};
 mock.operations = {};
 mock.keys = {};
 
-mock.keys.alpha = helpers.generateDidKeys();
+//mock.keys.alpha = helpers.generateDidKeys();
 //mock.keys.beta = helpers.generateDidKeys();
 //mock.keys.delta = helpers.generateDidKeys();
 //mock.keys.epsilon = helpers.generateDidKeys();
 //mock.keys.gamma = helpers.generateDidKeys();
 
-mock.didDescriptions.alpha = helpers.generateDid(mock.keys.alpha);
-//mock.didDescriptions.beta = helpers.generateDid(mock.keys.beta);
-//mock.didDescriptions.delta = helpers.generateDid(mock.keys.delta);
-//mock.didDescriptions.epsilon = helpers.generateDid(mock.keys.epsilon);
-//mock.didDescriptions.gamma = helpers.generateDid(mock.keys.gamma);
+(async function() {
+mock.didDocuments.alpha = await didv1.generate({passphrase: null});
+})();
 
 mock.operations.create = {
   '@context': constants.WEB_LEDGER_CONTEXT_V1_URL,
