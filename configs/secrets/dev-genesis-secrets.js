045cbd49defd4ca48d357558088057248397b9ec
David I. Lehn <dlehn@digitalbazaar.com>
2018-02-23 20:17:25 -0500

    Add accelerator support.
    - Add accelerator config values and enabled flag.
    - Generate DID if needed.
    - Add endpoint.
    - Update dependencies.

--- configs/secrets/dev-genesis-secrets.js
+++ configs/secrets/dev-genesis-secrets.js
@@ -8,11 +8,22 @@ const path = require('path');
 config['veres-one'].adminPassphrase =
   'password';
 config['veres-one'].peers = [];
+
+// maintainer
 config['veres-one'].maintainerConfigFile =
   path.join(config.paths.secrets, 'maintainer.ddo');
-config['veres-one'].governorsConfigFile =
-  path.join(config.paths.secrets, 'governors.ddo');
 config['veres-one'].maintainerPassphrase =
   'insecure_eyium0phookoh9geeshewomaekuoTeib';
+
+// governors
+config['veres-one'].governorsConfigFile =
+  path.join(config.paths.secrets, 'governors.ddo');
 config['veres-one'].governorsPassphrase =
   'insecure_shee0iyeifah0Zeew1bufeech4vo8wah';
+
+// accelerator
+config['veres-one'].acceleratorEnabled = true;
+config['veres-one'].acceleratorConfigFile = 
+  path.join(config.paths.secrets, 'accelerator.ddo');
+config['veres-one'].acceleratorPassphrase =
+  'insecure_Gx77CpVMq6Lmyq8452Z3o0TmtrcDnUWH';
