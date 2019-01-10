a5924c3a8f26545529f8f02683f23c73e494d173
Matthew Collier <collier.matthew@gmail.com>
2019-01-10 12:11:51 -0500

    Update test passing.

--- test/mocha/30-update.js
+++ test/mocha/30-update.js
@@ -6,7 +6,7 @@
 const didVeresOne = require('did-veres-one');
 const helpers = require('./helpers');
 
-describe('DID update', () => {
+describe.only('DID update', () => {
   it('a DID owner should be able to update its own DID document', async () => {
     const hostname = 'genesis.veres.one.localhost:23443';
     const v1 = didVeresOne.veres({
@@ -15,9 +15,10 @@ describe('DID update', () => {
     });
     let error;
     let did;
+    let didDocument;
     try {
       // Generate a new DID Document, store the private keys locally
-      const didDocument = await v1.generate({});
+      didDocument = await v1.generate({});
       console.log('Generated:', JSON.stringify(didDocument, null, 2));
       // Now register the newly generated DID Document
       // Use Equihash Proof of Work by default (see below)
@@ -47,5 +48,57 @@ describe('DID update', () => {
     }
     didRecord.record.id.should.equal(did);
     didRecord.meta.sequence.should.equal(0);
+    // create a patch
+    didDocument.observe();
+    const mockService = {
+      id: 'urn:foo',
+      type: 'AgentService',
+      serviceEndpoint: 'https://agent.example.com/'
+    };
+    didDocument.addService(mockService);
+    let updateResult;
+    try {
+      updateResult = await v1.update({didDocument});
+    } catch(e) {
+      // it's an axios error with a response
+      if(e.response && e.response.data) {
+        console.log('HTTP ERROR', JSON.stringify(e.response.data, null, 2));
+      } else {
+        console.log('ERROR', e);
+      }
+      error = e;
+    }
+    // FIXME v1.update should not be surfacing axios response
+    updateResult.status.should.equal(204);
+    should.not.exist(error);
+    const updatedDid = await waitForUpdate({did, hostname, sequence: 1});
+    const {meta: {sequence}, record} = updatedDid;
+    sequence.should.equal(1);
+    should.exist(record.service);
+    record.service.should.be.an('array');
+    record.service.should.have.length(1);
+    record.service[0].should.eql(mockService);
   });
 });
+
+async function waitForUpdate({did, hostname, sequence}) {
+  let found = false;
+  let didRecord;
+  while(!found) {
+    try {
+      didRecord = await helpers.getDid({did, hostname});
+      const {meta: {sequence: s}} = didRecord;
+      if(s === sequence) {
+        found = true;
+      }
+    } catch(e) {
+      if(e.response.status !== 404) {
+        throw e;
+      }
+      console.log('Waiting for consensus...');
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      continue;
+    }
+  }
+  return didRecord;
+}
