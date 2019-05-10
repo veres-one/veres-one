0956a36688a8f5ab822f198e4cb9a3811ada3108
Matthew Collier <mcollier@digitalbazaar.com>
2019-05-10 13:41:48 -0400

    Add product-config.

---
+++ configs/product-config.js
@@ -0,0 +1,7 @@
+/*!
+ * Copyright (c) 2017-2019 Digital Bazaar, Inc. All rights reserved.
+ */
+
+// for production, this file will be overwritten by packager
+
+require('./secrets/dev-genesis-secrets');
