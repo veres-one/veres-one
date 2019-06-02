634f035450da6a941d16c53133e037750f51b6bf
Matthew Collier <mcollier@digitalbazaar.com>
2019-06-02 08:27:22 -0400

    Disable express sessions.

--- configs/express.js
+++ configs/express.js
@@ -6,6 +6,9 @@
 const {config} = require('bedrock');
 require('bedrock-express');
 
+// disable sessions server wide
+config.express.useSession = false;
+
 // express info
 config.express.session.secret = 'NOTASECRET';
 config.express.session.key = 'veres-one.sid';
