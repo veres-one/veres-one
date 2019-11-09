4f5a98b1f99cd810a802c7cd0b768b8a88d15577
Manu Sporny <msporny@digitalbazaar.com>
2019-11-09 10:26:04 -0500

    Move database configs into database config files.

--- docker.js
+++ docker.js
@@ -4,13 +4,16 @@
 const bedrock = require('bedrock');
 const {config} = bedrock;
 const path = require('path');
+const process = require('process');
 
+// load configs from /etc/veres-one
 config.paths.config = path.join('/etc', 'veres-one');
-require('./lib/index');
 
-// use docker specific database hosts
-config.mongodb.host = 'mongo';
-config.redis.host = 'redis';
-config.jobs.queueOptions.redis.host = config.redis.host;
+// ensure that files in /etc/veres-one can require files in app directory
+process.env.NODE_PATH = '/home/node/app/node_modules';
+require('module').Module._initPaths();
+
+// load system and base configs
+require('./lib/index');
 
 bedrock.start();
