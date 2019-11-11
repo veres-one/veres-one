48c0d1433067cdd4961de4a9f0785a7de0835530
Matthew Collier <mcollier@digitalbazaar.com>
2019-11-11 16:29:38 -0500

    Define config.paths.config.

--- dev.js
+++ dev.js
@@ -2,6 +2,8 @@
  * Copyright (c) 2017-2019 Digital Bazaar, Inc. All rights reserved.
  */
 const bedrock = require('bedrock');
+const {config} = bedrock;
+const path = require('path');
 
 config.paths.config = path.join(__dirname, 'configs');
 require('./lib/index');
