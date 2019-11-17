04054dbbf7729cb863aebe6c9b3363f087e1cb66
Manu Sporny <msporny@digitalbazaar.com>
2019-11-17 16:53:50 -0500

    Remove old config files.

--- testnet.veres.one.js
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
- */
-var bedrock = require('bedrock');
-
-require('./lib/index');
-require('./configs/testnet');
-
-bedrock.start();
