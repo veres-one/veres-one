bb3d50bfaa6b86673201e3f54e9fa658efe71c1a
Manu Sporny <msporny@digitalbazaar.com>
2020-02-13 11:10:48 -0500

    Change node names for dev mode.

--- dev.js
+++ dev.js
@@ -8,4 +8,8 @@ const path = require('path');
 config.paths.config = path.join(__dirname, 'configs');
 require('./lib/index');
 
+config.server.domain = 'node-1.veres.one.local';
+config.server.host = config.server.domain;
+config.server.baseUri = 'https://' + config.server.domain;
+
 bedrock.start();
