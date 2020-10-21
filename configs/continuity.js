e2ccaa34db9196e8e48d7f59eb7e90170070fec7
Ganesh Annan <gannan@digitalbazaar.com>
2020-10-21 02:09:12 -0400

    Enable configuration of gossip max events.

--- configs/continuity.js
+++ configs/continuity.js
@@ -9,9 +9,11 @@ const cfg = config['ledger-consensus-continuity'];
 const {
   env: {
     ENABLE_CONSENUS_WORKERPOOL,
+    GOSSIP_MAX_EVENTS,
     WRITER_MAX_EVENTS
   }
 } = process;
 
 cfg.consensus.workerpool.enabled = ENABLE_CONSENUS_WORKERPOOL !== 'false';
+cfg.gossip.maxEvents = parseInt(GOSSIP_MAX_EVENTS, 10) || 100;
 cfg.writer.maxEvents = parseInt(WRITER_MAX_EVENTS, 10) || cfg.writer.maxEvents;
