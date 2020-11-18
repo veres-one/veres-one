1836065079be6375573fe383dc02d530287a7fab
Matthew Collier <mcollier@digitalbazaar.com>
2020-11-17 19:16:56 -0500

    Implement gossip event worker config.

--- configs/continuity.js
+++ configs/continuity.js
@@ -10,10 +10,16 @@ const {
   env: {
     ENABLE_CONSENUS_WORKERPOOL,
     GOSSIP_MAX_EVENTS,
-    WRITER_MAX_EVENTS
+    VERES_ONE_ENABLE_GOSSIP_EVENTS_WORKER,
+    VERES_ONE_MAX_CONCURRENT_EVENTS_PER_WORKER,
+    WRITER_MAX_EVENTS,
   }
 } = process;
 
 cfg.consensus.workerpool.enabled = ENABLE_CONSENUS_WORKERPOOL !== 'false';
 cfg.gossip.maxEvents = parseInt(GOSSIP_MAX_EVENTS, 10) || cfg.gossip.maxEvents;
 cfg.writer.maxEvents = parseInt(WRITER_MAX_EVENTS, 10) || cfg.writer.maxEvents;
+
+cfg.gossip.batchProcess.enable = !!VERES_ONE_ENABLE_GOSSIP_EVENTS_WORKER;
+cfg.gossip.batchProcess.concurrentEventsPerWorker =
+  !!VERES_ONE_MAX_CONCURRENT_EVENTS_PER_WORKER;
