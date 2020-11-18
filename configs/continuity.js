5bc2846da8f5d88fd98702a0132d6658271eae21
Ganesh Annan <gannan@digitalbazaar.com>
2020-11-17 21:17:05 -0500

    Remove unused config.

--- configs/continuity.js
+++ configs/continuity.js
@@ -10,8 +10,8 @@ const {
   env: {
     ENABLE_CONSENUS_WORKERPOOL,
     GOSSIP_MAX_EVENTS,
-    VERES_ONE_ENABLE_GOSSIP_EVENTS_WORKER,
-    VERES_ONE_MAX_CONCURRENT_EVENTS_PER_WORKER,
+    // VERES_ONE_ENABLE_GOSSIP_EVENTS_WORKER,
+    // VERES_ONE_MAX_CONCURRENT_EVENTS_PER_WORKER,
     WRITER_MAX_EVENTS,
   }
 } = process;
@@ -20,9 +20,8 @@ cfg.consensus.workerpool.enabled = ENABLE_CONSENUS_WORKERPOOL !== 'false';
 cfg.gossip.maxEvents = parseInt(GOSSIP_MAX_EVENTS, 10) || cfg.gossip.maxEvents;
 cfg.writer.maxEvents = parseInt(WRITER_MAX_EVENTS, 10) || cfg.writer.maxEvents;
 
-cfg.gossip.batchProcess.enable = !!VERES_ONE_ENABLE_GOSSIP_EVENTS_WORKER;
-if(VERES_ONE_MAX_CONCURRENT_EVENTS_PER_WORKER) {
-  cfg.gossip.batchProcess.concurrentEventsPerWorker =
-    parseInt(VERES_ONE_MAX_CONCURRENT_EVENTS_PER_WORKER, 10);
-}
-
+// cfg.gossip.batchProcess.enable = !!VERES_ONE_ENABLE_GOSSIP_EVENTS_WORKER;
+// if(VERES_ONE_MAX_CONCURRENT_EVENTS_PER_WORKER) {
+//   cfg.gossip.batchProcess.concurrentEventsPerWorker =
+//     parseInt(VERES_ONE_MAX_CONCURRENT_EVENTS_PER_WORKER, 10);
+// }
