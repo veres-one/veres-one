ef3aef7884ea2567a53309ea8703fa3103d00f05
Ganesh Annan <gannan@digitalbazaar.com>
2021-01-20 13:05:28 -0500

    Make operation queue size configurable.

--- configs/continuity.js
+++ configs/continuity.js
@@ -12,12 +12,17 @@ const {
     GOSSIP_MAX_EVENTS,
     VERES_ONE_ENABLE_LOCAL_EVENTS_VALIDATION,
     WRITER_MAX_EVENTS,
+    MAX_LOCAL_OPERATIONS_LIST_LENGTH
   }
 } = process;
 
 cfg.consensus.workerpool.enabled = ENABLE_CONSENUS_WORKERPOOL === 'true';
 cfg.gossip.maxEvents = parseInt(GOSSIP_MAX_EVENTS, 10) || cfg.gossip.maxEvents;
 cfg.writer.maxEvents = parseInt(WRITER_MAX_EVENTS, 10) || cfg.writer.maxEvents;
+
+// setup max operation queue size
+cfg.operations.maxQueueSize = parseInt(MAX_LOCAL_OPERATIONS_LIST_LENGTH, 10) ||
+  cfg.operations.maxQueueSize;
 
 if(VERES_ONE_ENABLE_LOCAL_EVENTS_VALIDATION === 'true') {
   const host = process.env.VERES_ONE_GOSSIP_EVENTS_WORKER_SERVICE_HOST;
