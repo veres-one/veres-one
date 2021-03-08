f6525f3cc21cfed1b6d8a128bb6351f769d8396e
Ganesh Annan <gannan@digitalbazaar.com>
2021-03-08 17:00:21 -0500

    Remove workerpool configuration.

--- configs/continuity.js
+++
@@ -1,8 +0,0 @@
-/*!
- * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
- */
-const {config} = require('bedrock');
-
-const cfg = config['ledger-consensus-continuity'];
-
-cfg.consensus.workerpool.enabled = false;
